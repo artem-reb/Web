a8437ce642416ce83c0fd2b66f574a266c3bae9d
GStorm888 <denchikevstr@icloud.com>
2025-01-12 13:14:13 +0300

    Сделал скелет страницы про соника

--- server.py
+++ server.py
@@ -7,7 +7,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def ruwiki():
-    return render_template(R'ruwiki.html')
+    return render_template('ruwiki.html')
+
+
+@app.route('/hello')
+def hello():
+    return '<h1>Hello, World!<h1>'
+
+
+@app.route('/sonic')
+def sonic_article():
+    return render_template('sonic_article.html')
+
+@app.route('/max')
+def find_max():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    if a > b:
+        return f'<h1>The maximum number is {a}<h1>'
+    else:
+        return f'<h1>The maximum number is {b}<h1>'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
