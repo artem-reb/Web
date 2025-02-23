cc8e2972cadc62dc652bbf4cbabe5ba2f4d25f54
GStorm888 <denchikevstr@icloud.com>
2025-02-23 13:01:27 +0300

    Полностью сделал шаблонную статью

--- server.py
+++ server.py
@@ -17,7 +17,13 @@ def hello():
 
 @app.route('/sonic')
 def sonic_article():
-    return render_template('sonic_article.html')
+    title_article = "Ёж Соник"
+    text_article = """
+Соник — синий антропоморфный ёж, созданный художником Наото Осимой, программистом Юдзи Накой и дизайнером Хирокадзу Ясухарой. Во время разработки было предложено множество образов главного героя будущей игры, но разработчики остановились на ёжике синего цвета. Своё имя Соник получил за способность бегать на сверхзвуковых скоростях (англ. sonic — «звуковой; со скоростью звука»). Геймплей за Соника в большинстве игр серии Sonic the Hedgehog заключается в быстром прохождении уровней и битвах с врагами, для атаки которых Соник сворачивается в шар во время прыжка. Немаловажную роль для Соника играют золотые кольца, служащие ему в качестве защиты. Главным антагонистом героя является доктор Эггман, который хочет захватить мир и построить свою империю «Эггманленд».
+"""
+    article_image_title = "Соник"
+    article_image_path = "static/sonic.png"
+    return render_template('sonic_article.html', title_article=title_article, article_image_title=article_image_title, text_article=text_article, article_image_path=article_image_path)
 
 @app.route('/base')
 def base():
